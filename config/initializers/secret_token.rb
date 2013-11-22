4e7c755d931690cca50a4089bb225519f7e7bd30
Guillaume Garcia <gg@laguildeculinaire.com>
2013-11-22 14:58:40 -0500

    Init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Customers::Application.config.secret_token = '8183d033fb8283597658e4466b140ea2c78a14e8b12d3242c4bbc4edbcbb511fb74ec881073dc34cadacc52d047f1c8842ee5901b2ec48930fdb43ef8260f0e2'
