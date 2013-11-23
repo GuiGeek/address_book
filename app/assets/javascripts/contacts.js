521e61b2ca584bda9c5a45d1064252c2cddd4d7d
Guillaume Garcia <guillaume.garcia@me.com>
2013-11-23 18:27:24 -0500

    Set SemanticUI validation form

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -14,4 +14,42 @@ $(document).ready(function() {
     return false;
   });
   
+  /*
+   * contact form
+   */
+
+  $('.ui.form').form(
+  {
+    firstName: {
+      identifier : 'contact_first_name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your first name'
+        },
+		{
+		  type   : 'length[2]',
+          prompt : 'Your first name must be at least 2 characters'
+        }
+      ]
+    },
+    lastName: {
+      identifier  : 'contact_last_name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your last name'
+        },
+		{
+		  type   : 'length[2]',
+          prompt : 'Your last name must be at least 2 characters'
+        }
+      ]
+    }
+  },
+  {
+    inline : true,
+    on     : 'blur'
+  });
+
 });
