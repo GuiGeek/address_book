9243ef642a88617c2e341847632760e587bbac09
Guillaume Garcia <gg@laguildeculinaire.com>
2013-11-25 15:10:24 -0500

    Remove pre build address on edit contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -36,7 +36,6 @@ class ContactsController < ApplicationController
   # GET /contacts/1/edit
   def edit
     @contact = Contact.find(params[:id])
-    @contact.addresses.build
   end
 
   # POST /contacts
