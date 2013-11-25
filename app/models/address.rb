e2b4791453c0874c568f362b9638d8c79f222e01
Guillaume Garcia <gg@laguildeculinaire.com>
2013-11-25 14:14:23 -0500

    Add email, phones and addresses on the show contact page

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,13 @@ class Address < ActiveRecord::Base
   
   validates :label, :presence => true,
                     :length => { :minimum => 2 }
+                    
+  def full_address(html)
+    if html
+      "#{street1} #{street2}<br/>#{city} #{region}<br/>#{postal_code} #{country}".html_safe
+    else
+      "#{street1} #{street2} #{city} #{region} #{postal_code} #{country}"
+    end
+  end
+  
 end
